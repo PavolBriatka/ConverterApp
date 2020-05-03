979d53e6aade084a7102b4be5e9c8432924ff82f
PavolBriatka <pavol.briatka@redcloudtechnology.com>
2020-05-03 13:19:20 +0100

    rxdata: Implement simple interactor unit test

--- app/src/test/java/com/example/converterapp/repository/interactor/ConversionRatesInteractorTest.kt
+++ app/src/test/java/com/example/converterapp/repository/interactor/ConversionRatesInteractorTest.kt
@@ -1,13 +1,33 @@
 package com.example.converterapp.repository.interactor
 
-import com.example.converterapp.webservice.conversionratesinteractor.IConversionRatesInteractor
-import org.junit.Before
+import com.example.converterapp.webservice.EndpointDefinition
+import com.example.converterapp.webservice.conversionratesinteractor.ConversionRatesInteractor
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ConversionRatesInteractorTest {
 
-    private lateinit var interactor: IConversionRatesInteractor
+    @Mock
+    lateinit var endpointDefinition: EndpointDefinition
 
-    @Before
-    fun setUp() {
+    @InjectMocks
+    lateinit var interactor: ConversionRatesInteractor
+
+    @Test
+    fun fetchRates_correctEndpointCalled() {
+        //Arrange
+        //Act
+        interactor.fetchConversionRates("EUR")
+        //Assert
+        argumentCaptor<String>().apply {
+            verify(endpointDefinition).fetchConversionRates(capture())
+            assert(this.lastValue == "EUR")
+        }
     }
 }
